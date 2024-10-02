05143f2a17e3bf52c7adbcfafb557a9d07d91e20
Akshaya Barat Bushan <akshayabarat@gmail.com>
2024-10-02 00:32:33 -0400

    Time: 57 ms (17.68%), Space: 16.9 MB (52.1%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,18 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        # # Solution 1:
+        # return Counter(s) == Counter(t) #hash map but just counts
+        
+        # #Solution 2:
+        # return sorted(s) == sorted(t) -- Time - o(n log n) but memory - o(1)
+
+        if len(s) == len(t):
+            
+            countS, countT = {}, {} # hashmap
+            for i in range(len(s)):
+                countS[s[i]] =  1 + countS.get(s[i],0)
+                countT[t[i]] = countT.get(t[i],0) + 1
+            return countS == countT
+        return False
+
+
