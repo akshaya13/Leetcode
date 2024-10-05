1ece0f1cfc709e7d18d667c604eb7469a046b7df
Akshaya Barat Bushan <akshayabarat@gmail.com>
2024-10-04 20:05:55 -0400

    Time: 711 ms (74.8%), Space: 27.5 MB (46.36%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,24 +1,10 @@
-#######################################
-# Time Complexity  = O(n)
-# Space Complexity = O(1)
-# buy low, sell high 
-#check if Selling price is greater than cost price.
-#    1 - 7 = -6 (Here SP-CP= LOSS), so just update the Right pointer
-#    5 - 1 = 4  Here profit --> so find profit and update Max profit and update the Right pointer
-
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        #buy = left, sell = right 
-        l, r = 0, 1  # 0 and 1 are indices
-        maxProfit = 0
-        while r < len(prices): # iterating the list           
-            if prices[l] < prices[r]:
-                curProfit = prices[r] - prices[l]
-                maxProfit = max(maxProfit, curProfit)
-            else: 
-                l = r
-            
-            r += 1 
+        profit = 0
 
-        return maxProfit
-        
+        lowest = prices[0]
+        for price in prices:
+            if price < lowest:
+                lowest = price
+            profit = max(profit, price-lowest)
+        return profit
