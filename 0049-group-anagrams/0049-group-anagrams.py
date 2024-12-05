2c0a870ff900f500589dcc33b0dde22529faa776
Akshaya Barat Bushan <akshayabarat@gmail.com>
2024-12-05 15:08:17 -0500

    Time: 23 ms (20.04%), Space: 22.1 MB (11.57%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,32 +1,21 @@
-# class Solution:
-#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-#         hmap = defaultdict(list)
-#         count =[0]*26 # 26 chars of alphabet
-        
-#         for string in strs:
-#             for char in string:
-#                 count[ord(char)-ord("a")] += 1
-            
-#             # ans[tuple(count)].append(string)
-#             hmap[tuple(count)].append(string)
-
-#         return hmap.values()
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        #res = {} #hashmap -- map character counts to the strings
-        ans = defaultdict(list) #default value is a list
-        # 1e, 1a, 1t ---> eat, ate, tea
-        #hat = [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0] 
-       
-        for string in strs:
-            count=[0]*26 #count array of len of 26 -- a to z [0,0,0,0,0,0,0....]
-
-            for char in string:
-                count[ord(char)- ord("a")] += 1 # ascii any char - Ascii A == 80-80 = 0
-            
-            #ans[count].append but list cannot be keys. Tuples - non mutable 
-           # {[1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0] : hat}
-            ans[tuple(count)].append(string)
         
-        return ans.values()
+        # result = defaultdict(list)
+        
+        # for string in strs:
+        #     count = [0] * 26
+        #     for char in string:
+        #         count[ord(char)-ord('a')] += 1
+        #     result[tuple(count)].append(string)
+        
+        # return result.values()
+        result = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+            for char in s:
+                count[ord(char) - ord('a')] += 1
+            result[tuple(count)].append(s)
+
+        return list(result.values())
