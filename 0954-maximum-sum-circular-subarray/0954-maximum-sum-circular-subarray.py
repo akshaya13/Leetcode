f37940375fda689c1675c565fda76e86a43af501
Akshaya Barat Bushan <akshayabarat@gmail.com>
2024-12-14 19:27:37 -0500

    Time: 103 ms (40.99%), Space: 21.4 MB (11.68%) - LeetHub

--- 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.py
+++ 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.py
@@ -1,28 +1,22 @@
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
-      # Helper function to find the maximum subarray sum using Kadane's algorithm
-        def kadane(nums):
-            curMax = nums[0]
-            maxSoFar = nums[0]
-            curMin = nums[0]
-            minsofar = nums[0]
+        curMax = nums[0]
+        gmax = nums[0]
+        curMin = nums[0]
+        gmin = nums[0]
+        total = nums[0]
 
-            for num in nums[1:]:
-                curMax = max(curMax + num, num)
-                curMin = min(curMin + num, num)
-                maxSoFar = max(maxSoFar, curMax)
-                minsofar = min(minsofar, curMin)
-            return maxSoFar, minsofar
+        for num in nums[1:]:
+            total += num
+            curMax = max(curMax + num, num)
+            curMin = min(curMin + num, num)
+            gmax  = max(gmax, curMax)
+            gmin = min(gmin, curMin)
+        
 
-        # Total sum of the array
-        totalSum = sum(nums)
+        final = total - gmin
 
-        # Maximum subarray sum (non-circular case)
-        maxSum, minsum = kadane(nums)
-
-        final = totalSum - minsum
-        if maxSum < 0:
-            return maxSum
-
-        # Return the maximum of non-circular and circular cases
-        return max(maxSum, final)
+        if gmax  < 0:
+            return gmax 
+    
+        return max(gmax, final)
